967b88a2c4681eccc1cb818cc2c77cefcdd2b503
jorgen1999 <117076544+jorgen1999@users.noreply.github.com>
2022-11-02 12:23:44 +0100

    Add files via upload

---
+++ mysite/mysite/views.py
@@ -0,0 +1,148 @@
+from .models import Car
+from .models import Customer
+from .models import Employee
+from .serializer import CustomerSerializer
+from .serializer import EmployeeSerializer
+from rest_framework.response import Response
+from .serializer import CarSerializer
+from rest_framework import status
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+
+
+
+@api_view(["GET"])
+def get_cars(request):
+    cars = Car.objects.all()
+    serializer = CarSerializer(cars, many=True)
+    print(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(["POST"])
+def save_car(request):
+    serializer = CarSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(["PUT"])
+def update_car(request, id):
+    try:
+        theCar = Car.objects.get(pk=id)
+    except Car.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = CarSerializer(theCar, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["DELETE"])
+def delete_car(request, id):
+    try:
+        theCar = Car.objects.get(pk=id)
+    except Car.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    theCar.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+#-------------------------------------------------------------------------
+@api_view(["GET"])
+def get_customer(request):
+    cars = Customer.objects.all()
+    serializer = CustomerSerializer(cars, many=True)
+    print(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(["POST"])
+def save_customer(request):
+    serializer = CustomerSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(["PUT"])
+def update_customer(request, id):
+    try:
+        theCar = Customer.objects.get(pk=id)
+    except Customer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = CustomerSerializer(theCar, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["DELETE"])
+def delete_customer(request, id):
+    try:
+        theCar = Customer.objects.get(pk=id)
+    except Customer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    theCar.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+#-----------------------------------------------------------------------
+
+@api_view(["GET"])
+def get_employee(request):
+    cars = Employee.objects.all()
+    serializer = EmployeeSerializer(cars, many=True)
+    print(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(["POST"])
+def save_employee(request):
+    serializer = EmployeeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(["PUT"])
+def update_employee(request, id):
+    try:
+        theCar = Employee.objects.get(pk=id)
+    except Employee.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = EmployeeSerializer(theCar, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["DELETE"])
+def delete_employee(request, id):
+    try:
+        theCar = Employee.objects.get(pk=id)
+    except Employee.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    theCar.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(["GET"])
+def order_car(request, id):
+    theCustomer = Customer.objects.get(pk=id)
+    theCar = Car.objects.get(pk=id)
+    serializer = CustomerSerializer(theCustomer)
+    serializers = CarSerializer(theCar)
+
+    return Response(serializers.data, status=status.HTTP_200_OK)
+
+
+
+
+
+
+
+
+
+
+
