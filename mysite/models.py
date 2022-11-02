412da313aac4641098aa247e7d9f88051331c09e
jorgen1999 <117076544+jorgen1999@users.noreply.github.com>
2022-11-02 13:08:19 +0100

    Add files via upload

---
+++ mysite/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+class Car(models.Model):
+    make = models.CharField(max_length=50)
+    carmodel = models.CharField(max_length=50)
+    year = models.CharField(max_length=50, default="0")
+    location = models.CharField(max_length=50, default= None)
+    status = models.CharField(max_length=50, default= None)
+
+
+    def __str__(self):
+        return self.make + ' ' + self.carmodel + ' ' + self.year + ' ' + self.location + ' ' + self.status
+
+    def is_available(self):
+        status="booked"
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name + ' ' + self.age + ' ' + self.address
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    branch = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name + ' ' + self.address + ' ' + self.branch
